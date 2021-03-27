fee2f21d32377df75db3549e7b8f9b37d1a348e8
Diana Turnea <turnea.diana@gmail.com>
2021-03-27 12:16:52 +0200

    API Server

--- services/list.js
+++ services/list.js
@@ -1,3 +1,3 @@
-export function getList() {
-  return fetch("http://localhost:3333/list").then((data) => data.json());
-}
+// export function getList() {
+//   return fetch("http://localhost:3333/list").then((data) => data.json());
+// }
